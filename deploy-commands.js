a4490165f020dfa83efe7445d9f9d0a030aee26c
Ramy Ouda <ramyouda@gmail.com>
2025-01-03 23:31:20 -0500

    initial commit

---
+++ deploy-commands.js
@@ -0,0 +1,55 @@
+const { REST, Routes } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commands = [];
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+commandFolders.forEach((folder) => {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  commandFiles.forEach((file) => {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.warn(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  });
+});
+
+const rest = new REST().setToken(token);
+
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    const restCommands = {
+      globalCommands: (commands) =>
+        rest.put(Routes.applicationCommands(clientId), { body: commands }),
+      privateCommands: (commands) =>
+        rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+          body: commands,
+        }),
+    };
+
+    const data = await restCommands.globalCommands(commands);
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
